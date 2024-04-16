a9aed12b4cc9b5255dbdaf1b066d47650b596bff
akyarv0 <akyarv0@gmail.com>
2024-04-16 20:14:27 +0300

    scss changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.scss';
+import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
    <div>
    <Navbar />
+   <Home />
+   <Footer />
+  
+
    </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './_nav.scss'
 
-import './Navbar.scss';
+
+
 
 function Navbar() {
   return (
-    <nav className="nav">
-      <ul className="nav__list">
-        <li className="nav__item">
-          <NavLink exact to="/" className="nav__link" activeClassName="nav__link--active">Home</NavLink>
-        </li>
-        <li className="nav__item">
-          <NavLink to="/about" className="nav__link" activeClassName="nav__link--active">About Me</NavLink>
-        </li>
-        <li className="nav__item">
-          <NavLink to="/projects" className="nav__link" activeClassName="nav__link--active">My Project</NavLink>
-        </li>
-        <li className="nav__item">
-          <NavLink to="/contact" className="nav__link" activeClassName="nav__link--active">Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div>
+    <ul class="nav__list">
+    <li class="nav__item">
+        <a href="index.html" class="nav__link nav__link--active">Home</a>
+    </li>
+    <li class="nav__item">
+        <a href="about.html" class="nav__link">About Me</a>
+    </li>
+    <li class="nav__item">
+        <a href="projects.html" class="nav__link">My Project</a>
+    </li>
+    <li class="nav__item">
+        <a href="contact.html" class="nav__link">Contact</a>
+    </li>
+</ul>
+    </div>
   );
 }
 
