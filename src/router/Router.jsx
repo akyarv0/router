494cd33f58ed7f743ed5ace748a88ed6ca9ffbde
akyarv0 <akyarv0@gmail.com>
2024-04-19 23:49:11 +0300

    router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "../pages/home/Home";
 import Navbar from "../components/Navbar/Navbar";
 import Contact from "../pages/contact/Contact";
 import About from "../pages/about/About";
 import Projects from "../pages/projects/Projects";
 import NotFound from "../components/Error/NotFound";
-
 import React from "react";
 import Footer from "../components/Footer/Footer";
 
@@ -16,13 +14,12 @@ const Router = () => {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<><Home /><Footer /></>} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/about" element={<><About /><Footer /></>} />
+        <Route path="/projects" element={<><Projects /><Footer /></>} />
+        <Route path="*" element={<><NotFound /><Footer /></>} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
